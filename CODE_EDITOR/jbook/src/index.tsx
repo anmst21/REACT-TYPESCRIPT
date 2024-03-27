b633f51601841bad30b59c4c94decd71b95cae5a
anmst21 <anmstudios21c@gmail.com>
2024-03-27 15:01:18 -0400

    added codeeditor component and styling

--- CODE_EDITOR/jbook/src/index.tsx
+++ CODE_EDITOR/jbook/src/index.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from "./components/code-editor";
 
 const el = document.getElementById("root");
 
@@ -71,6 +72,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor initialValue={"cock"} onChange={(value) => setInput(value)} />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
